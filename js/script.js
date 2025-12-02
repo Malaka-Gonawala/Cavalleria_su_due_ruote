80d9c014b1824691ef366f802c4ab7eeedbcf1fc
Malaka-Gonawala <malakagonawala123@gmail.com>
2025-12-02 21:48:42 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,22 +3,22 @@ let nav = document.querySelector("#Nav");
 let navOpened = false;
 
 function toggleNav() {
-  nav.classList.toggle("open");
-  btnOpenNav.classList.toggle("active");
-  navOpened = nav.classList.contains("open");
-  fixTitle();
+    nav.classList.toggle("open");
+    btnOpenNav.classList.toggle("active");
+    navOpened = nav.classList.contains("open");
+    fixTitle();
 }
 btnOpenNav.addEventListener("click", toggleNav);
 
 window.addEventListener("click", function (e) {
-  if (nav.classList.contains("open")) {
-    if (!nav.contains(e.target) && !btnOpenNav.contains(e.target)) {
-      nav.classList.remove("open");
-      btnOpenNav.classList.remove("active");
-      navOpened = false;
-      fixTitle();
+    if (nav.classList.contains("open")) {
+        if (!nav.contains(e.target) && !btnOpenNav.contains(e.target)) {
+            nav.classList.remove("open");
+            btnOpenNav.classList.remove("active");
+            navOpened = false;
+            fixTitle();
+        }
     }
-  }
 });
 
 // --------------------------------------------
@@ -26,36 +26,38 @@ window.addEventListener("click", function (e) {
 let title = document.querySelector("#Title-Container");
 let scrollTimeout;
 let minScrollHeight;
-let loc = window.location.pathname;
-console.log(loc)
+let loc = window.location.href;
+console.log(loc);
 
-if (loc.includes("/html/modelli.html") ||
-    loc.includes("/html/modelli_focus.html")) {
-  minScrollHeight = title.scrollHeight * 2;
+if (
+    loc.includes("/html/modelli.html") ||
+    loc.includes("/html/modelli_focus.html")
+) {
+    minScrollHeight = title.scrollHeight * 2.5;
 } else {
-  minScrollHeight = title.scrollHeight;
+    minScrollHeight = title.scrollHeight;
 }
 
 const fixTitle = () => {
-  if (window.scrollY < minScrollHeight) {
-    clearTimeout(scrollTimeout);
-    title.classList.remove("start-scroll");
+    if (window.scrollY < minScrollHeight) {
+        clearTimeout(scrollTimeout);
+        title.classList.remove("start-scroll");
+        title.style.opacity = 1;
+        title.style.pointerEvents = "auto";
+        return;
+    }
+    title.classList.add("start-scroll");
     title.style.opacity = 1;
     title.style.pointerEvents = "auto";
-    return;
-  }
-  title.classList.add("start-scroll");
-  title.style.opacity = 1;
-  title.style.pointerEvents = "auto";
 
-  clearTimeout(scrollTimeout);
+    clearTimeout(scrollTimeout);
 
-  if (navOpened === false) {
-    scrollTimeout = setTimeout(() => {
-      title.style.pointerEvents = "none";
-      title.style.opacity = 0;
-    }, 1000);
-  }
+    if (navOpened === false) {
+        scrollTimeout = setTimeout(() => {
+            title.style.pointerEvents = "none";
+            title.style.opacity = 0;
+        }, 1000);
+    }
 };
 
 window.addEventListener("scroll", fixTitle);
